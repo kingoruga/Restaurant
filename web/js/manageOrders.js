a983cea022a7038cd5afe74a607807de389f6db3
Dilan Jenkins <djenkins1@email.arizona.edu>
2018-01-29 09:56:30 -0700

    Add files via upload

--- web/js/manageOrders.js
+++ web/js/manageOrders.js
@@ -1,13 +1,24 @@
 //renders out a table row for the order given as a prop
 OrderRow = React.createClass({
+   renderAddress : function()
+   {
+        return (
+            <td>
+                {this.props.order.address.street},
+                &nbsp;{this.props.order.address.city},
+                &nbsp;{this.props.order.address.state},
+                &nbsp;{this.props.order.address.zipcode}
+            </td>
+         );
+   },
    render: function()
    {
        return (
             <tr>
                 <td>{this.props.order.orderid}</td>
-                <td>{this.props.order.email}</td>
-                <td>{this.props.order.name}</td>
-                <td>{this.props.order.address}</td>
+                <td>{this.props.order.user.email}</td>
+                <td>{this.props.order.user.name}</td>
+                {this.renderAddress()}
                 <td>{this.props.order.price}</td>
                 <td>{this.props.order.payment}</td>
             </tr>    
@@ -23,44 +34,27 @@ OrderTable = React.createClass({
             <OrderRow key={index} order={order} />
         );
     },
-    handleClickSort: function( event )
-    {
-        event.preventDefault();
-        //gets the data-column attribute from the html
-        var columnSortBy = event.target.dataset.column;
-        //TODO: need to either sort the data client side or send a new ajax request
-        //TODO: need to propagate this event up to parent component that has state
-        //TODO: remove the following when implemented
-        console.log( columnSortBy );
-        alert( "CLICKED" );
-    },
     renderTableHeader: function()
     {
         return (
             <thead className="thead-dark">
                 <th>Order ID 
-                    &nbsp;<a href='#' data-column="orderid"
-                        onClick={this.handleClickSort.bind( this )}>&#8595;</a>
+                    &nbsp;<a href='?col=order_id'>&#8595;</a>
                 </th>
                 <th>Email
-                    &nbsp;<a href='#' data-column="email"
-                        onClick={this.handleClickSort.bind( this )}>&#8595;</a>
+                    &nbsp;<a href='?col=email'>&#8595;</a>
                 </th>
                 <th>Name 
-                    &nbsp;<a href='#' data-column="name"
-                        onClick={this.handleClickSort.bind( this )}>&#8595;</a>
+                    &nbsp;<a href='?col=last_name'>&#8595;</a>
                 </th>
                 <th>Address 
-                    &nbsp;<a href='#' data-column="address"
-                        onClick={this.handleClickSort.bind( this )}>&#8595;</a>
+                    &nbsp;<a href='?col=zip_code'>&#8595;</a>
                 </th>
                 <th>Price 
-                    &nbsp;<a href='#' data-column="price"
-                        onClick={this.handleClickSort.bind( this )}>&#8595;</a>
+                    &nbsp;<a href='?col=price'>&#8595;</a>
                 </th>
                 <th>Payment 
-                    &nbsp;<a href='#' data-column="payment"
-                        onClick={this.handleClickSort.bind( this )}>&#8595;</a>
+                    &nbsp;<a href='?col=payment_type'>&#8595;</a>
                 </th>
             </thead> 
         );
